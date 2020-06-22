a8e07c739fb86170a865f30a26d3c2f5f7bc9599
Baraja Swargiary <barajaswargiary4444@gmail.com>
2020-06-22 13:45:02 +0530

    updated menu items and added onclick events to navigate withRouter() HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
 import './App.css'
 
 import HomePage from './pages/homepage/homepage.component.jsx'
 
+const HatsPage = props => {
+	console.log(props.match)
+	return (
+		<div>
+			<h1>Hats Page</h1>
+		</div>
+	)
+}
+
 function App() {
 	return (
 		<div className='App'>
-			<HomePage />
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route path='/hats' component={HatsPage} />
+			</Switch>
 		</div>
 	)
 }
